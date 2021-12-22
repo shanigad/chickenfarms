5b4870234653dd6a154e0a12a61380330dc30dac
sgad <shani_gad@intuit.com>
2021-12-22 13:38:59 +0200

    Add problem fixtures

---
+++ customer-escalation-management/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO problem (id,name) VALUES (101,'General Error');
+INSERT IGNORE INTO problem (id,name) VALUES (102,'Chicken is dead');
+INSERT IGNORE INTO problem (id,name) VALUES (103,'Invalid Chicken');
---
+++ customer-escalation-management/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `problem` (
+                          `id` int NOT NULL,
+                          `name` varchar(50) DEFAULT NULL,
+                          PRIMARY KEY (`id`)
+);
