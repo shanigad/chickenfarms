585204347ea0cb0d088601703bd96e024ddddf0b
sgad <shani_gad@intuit.com>
2021-12-23 14:53:26 +0200

    Add root cause fixtures

--- customer-escalation-management/src/main/resources/data.sql
+++ customer-escalation-management/src/main/resources/data.sql
@@ -1,3 +1,11 @@
-INSERT IGNORE INTO problem (id,name) VALUES (101,'General Error');
-INSERT IGNORE INTO problem (id,name) VALUES (102,'Chicken is dead');
-INSERT IGNORE INTO problem (id,name) VALUES (103,'Invalid Chicken');
+INSERT IGNORE INTO problem (id,name) VALUES (101,'General Error'),
+                                            (102,'Missing Chicken'),
+                                            (103,'Invalid Chicken'),
+                                            (104,'Wrong Chicken'),
+                                            (105,'Broken Egg');
+
+INSERT IGNORE INTO root_cause (rc_id,name) VALUES (201,'Chicken is dead'),
+                                            (202,'Farm is closed'),
+                                            (203,'The dog eat my homework'),
+                                            (204,'System error'),
+                                            (205,'New farm layout');
--- customer-escalation-management/src/main/resources/schema.sql
+++ customer-escalation-management/src/main/resources/schema.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS `problem` (
                           `name` varchar(50) DEFAULT NULL,
                           PRIMARY KEY (`id`)
 );
+CREATE TABLE IF NOT EXISTS `root_cause` (
+                                         `rc_id` int NOT NULL,
+                                         `name` varchar(50) DEFAULT NULL,
+                                         PRIMARY KEY (`rc_id`)
+);
