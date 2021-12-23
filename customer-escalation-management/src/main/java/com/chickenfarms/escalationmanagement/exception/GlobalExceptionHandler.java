e42df2ca7ff7b063a6fb1b1b1b90cd372fbe4b48
sgad <shani_gad@intuit.com>
2021-12-23 15:29:35 +0200

    Add argumentNotValid exception handling

--- customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/exception/GlobalExceptionHandler.java
+++ customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/exception/GlobalExceptionHandler.java
@@ -3,10 +3,15 @@ package com.chickenfarms.escalationmanagement.exception;
 
 import com.chickenfarms.escalationmanagement.model.dto.ErrorDetails;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
@@ -19,6 +24,19 @@ public class GlobalExceptionHandler {
         webRequest.getDescription(false));
 
     return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorDetails> handleValidationExceptions(MethodArgumentNotValidException exception,  WebRequest webRequest){
+
+    StringBuilder errors = new StringBuilder();
+    exception.getBindingResult().getAllErrors().forEach(violation ->
+        errors.append(" Field " + ((FieldError) violation).getField() + " throws an error: "+  violation.getDefaultMessage() + System.lineSeparator()));
+
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), errors.toString(),
+        webRequest.getDescription(false));
+
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
   }
 
 }
