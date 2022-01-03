9fbbeb529c85a9f40ef47de6133b4129566dbdfa
sgad <shani_gad@intuit.com>
2022-01-03 12:54:58 +0200

    Add MoveTicketToReady Tests

--- customer-escalation-management/src/test/java/com/chickenfarms/escalationmanagement/rest/service/TicketManagerServiceTest.java
+++ customer-escalation-management/src/test/java/com/chickenfarms/escalationmanagement/rest/service/TicketManagerServiceTest.java
@@ -2,14 +2,19 @@ package com.chickenfarms.escalationmanagement.rest.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import com.chickenfarms.escalationmanagement.enums.Status;
+import com.chickenfarms.escalationmanagement.exception.ChangeStatusException;
 import com.chickenfarms.escalationmanagement.exception.ResourceNotFoundException;
+import com.chickenfarms.escalationmanagement.model.entity.RootCause;
 import com.chickenfarms.escalationmanagement.model.entity.Ticket;
 import com.chickenfarms.escalationmanagement.repository.ProblemRepository;
+import com.chickenfarms.escalationmanagement.repository.RootCauseRepository;
 import com.chickenfarms.escalationmanagement.repository.TicketRepository;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,13 +29,11 @@ public class TicketManagerServiceTest {
  private CustomerService customerService;
  @Mock
  private ProblemRepository problemRepository;
+ @Mock
+ private RootCauseRepository rootCauseRepository;
  @InjectMocks
  private TicketManagerService ticketManagerService;
 
-
- @BeforeEach
- public void initTicketManager(){
- }
 
  @Test
  public void getTicketIfExistResourceNotFoundTest(){
@@ -48,5 +51,45 @@ public class TicketManagerServiceTest {
     when(ticketRepository.getById(anyLong())).thenReturn(ticket);
     assertThat(ticketManagerService.getTicketIfExist(1L)).isEqualTo(ticket);
   }
+
+
+  @Test
+  public void moveToReadyRootCauseNotFoundTest(){
+   when(rootCauseRepository.findById(anyInt())).thenReturn(Optional.empty());
+   assertThatExceptionOfType(ResourceNotFoundException.class).isThrownBy(() ->{
+    ticketManagerService.moveTicketToReady(1L, 1);
+   });
+  }
+
+ @Test
+ public void moveToReadyTicketNotFoundTest(){
+  when(rootCauseRepository.findById(anyInt())).thenReturn(Optional.of(new RootCause()));
+  when(ticketRepository.findById(anyLong())).thenReturn(Optional.empty());
+  assertThatExceptionOfType(ResourceNotFoundException.class).isThrownBy(() ->{
+   ticketManagerService.moveTicketToReady(1L, 1);
+  });
+ }
+
+ @Test
+ public void moveToReadyClosedTicketTest(){
+  Ticket ticket = new Ticket();
+  ticket.setStatus(Status.CLOSED.getStatus());
+  when(rootCauseRepository.findById(anyInt())).thenReturn(Optional.of(new RootCause()));
+  when(ticketRepository.findById(anyLong())).thenReturn(Optional.of(ticket));
+  assertThatExceptionOfType(ChangeStatusException.class).isThrownBy(() ->{
+   ticketManagerService.moveTicketToReady(1L, 1);
+  });
+ }
+
+// @Test
+// public void moveToReadyValidCreatedTicketTest(){
+//  Ticket ticket = new Ticket();
+//  ticket.setStatus(Status.CREATED.getStatus());
+//  when(rootCauseRepository.findById(anyInt())).thenReturn(Optional.of(new RootCause()));
+//  when(ticketRepository.findById(anyLong())).thenReturn(Optional.of(ticket));
+//  assertThatExceptionOfType(ChangeStatusException.class).isThrownBy(() ->{
+//   ticketManagerService.moveTicketToReady(1L, 1);
+//  });
+// }
 
 }
