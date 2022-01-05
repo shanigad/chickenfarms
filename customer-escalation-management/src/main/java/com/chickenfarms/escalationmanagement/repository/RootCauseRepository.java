cb2d104ebfa1fcdff89c5eb8615940b0b7f0db9b
sgad <shani_gad@intuit.com>
2022-01-05 16:04:44 +0200

    Add repository annotation to RootCauseRepo and TagRepo

--- customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/repository/RootCauseRepository.java
+++ customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/repository/RootCauseRepository.java
@@ -2,6 +2,8 @@ package com.chickenfarms.escalationmanagement.repository;
 
 import com.chickenfarms.escalationmanagement.model.entity.RootCause;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RootCauseRepository extends JpaRepository<RootCause, Long> {
 }
--- customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/repository/TagRepository.java
+++ customer-escalation-management/src/main/java/com/chickenfarms/escalationmanagement/repository/TagRepository.java
@@ -3,7 +3,9 @@ package com.chickenfarms.escalationmanagement.repository;
 import com.chickenfarms.escalationmanagement.model.entity.Tag;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
   Optional<Tag> findTagByName(String name);
 }
